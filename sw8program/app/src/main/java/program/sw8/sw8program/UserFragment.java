63618a642db4567adbd4fc85d6fcb4860d2db4c2
Carsten Holst <treeline@live.dk>
2015-02-26 12:02:57 +0100

    Profile picture can now be of any size, as application will resize it

--- sw8program/app/src/main/java/program/sw8/sw8program/UserFragment.java
+++ sw8program/app/src/main/java/program/sw8/sw8program/UserFragment.java
@@ -3,6 +3,8 @@ package program.sw8.sw8program;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -83,30 +85,31 @@ public class UserFragment extends Fragment {
         switch (requestCode) {
             case SelectImage:
                 //If we selected an image from gallery, get the) URI and do some string manipulation because fuck KitKat.
-                //TODO: Test on pre-KitKat phone
                 Uri uri = data.getData();
-                String pathsegment[] = uri.getLastPathSegment().split(":");
-                String id = pathsegment[1];
-                String[] imageColumns = {MediaStore.Images.Media.DATA};
-                uri = getUri();
-                Cursor imageCursor = getActivity().getContentResolver().query(uri, imageColumns, MediaStore.Images.Media._ID + "=" + id, null, null);
-
-                if (imageCursor.moveToFirst()) {
-                    uri = uri.parse(imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA)));
-                }
-                //Finally set the image based on the URI we derived
-                UserImage.setImageURI(uri);
+                //TODO: Target height and width is just random numbers. Figure out something proper.
+                UserImage.setImageBitmap(getResizedBitmap(10, 10, ImageFilePath.getPath(getActivity(), uri)));
                 break;
         }
     }
 
-    //Figure out where to look for images (Internal/External storage)
-    private Uri getUri() {
-        String state = Environment.getExternalStorageState();
-        if (state.equalsIgnoreCase(Environment.MEDIA_MOUNTED)) {
-            return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        } else {
-            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
-        }
+    public Bitmap getResizedBitmap(int targetW, int targetH,  String imagePath) {
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        //inJustDecodeBounds = true <-- will not load the bitmap into memory
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = 5;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
+        return(bitmap);
     }
 }
