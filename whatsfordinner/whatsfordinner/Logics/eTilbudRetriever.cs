89364c6ad0447c4da53187415551dfc21351f374
Morten <treeline@live.dk>
2015-04-09 09:43:17 +0200

    Updated eTilbudsReceiver

--- whatsfordinner/whatsfordinner/Logics/eTilbudRetriever.cs
+++ whatsfordinner/whatsfordinner/Logics/eTilbudRetriever.cs
@@ -8,7 +8,6 @@ using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography;
-using System.Text;
 
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
@@ -29,12 +28,13 @@ namespace whatsfordinner {
         readonly String ParamLongitude = "r_lng";
         readonly String ParamRadius = "r_radius";
         readonly String ParamLimit = "limit";
+        readonly String ParamOffset = "offset";
 
         //Parameter values
         String Latitude = "57.051188"; //Havnen i Aalborg
         String Longitude = "9.922371";
         String Radius = "800000"; //Radius in meters (800km)
-        String Limit = "50"; //Get 50 offers
+        String Limit = "48"; // Max amount of results in a query
 
         //Custom header identifiers
         readonly String HeaderXToken = "X-Token";
@@ -53,65 +53,96 @@ namespace whatsfordinner {
         //Keys
         readonly String ApiKey = "00i7q0nv9vp6kvbhzgr2ggsev4jrvy4f";
         readonly String ApiSecret = "00i7q0nv9vfaf8xgygg0qmxlkcwr3nvh";
-        String XToken = null; //Same as a Session Token in documentation
-        String XSignature = null; //Same as a Session Signature in documentation
+        String XToken; //Same as a Session Token in documentation
+        String XSignature; //Same as a Session Signature in documentation
 
         public eTilbudRetriever() {
 
+            //Create a body consisting of the API key
             List<KeyValuePair<String, String>> body = new List<KeyValuePair<String, String>>();
             body.Add(new KeyValuePair<String, String>(ParamApiKey, ApiKey));
+
+            //Send request to create a new session
             String response = SendWebRequest(Post, Sessions, body);
+
+            //Get the Session Token from the response
             dynamic json = JObject.Parse(response);
             XToken = json.token;
+
+            //Save the Session Signature as well (SHA256 version of API Secret combined with Session Token)
             XSignature = ConvertToSha256(ApiSecret + XToken);
             
         }
 
-        public String GetOfferList() {
-
-            List<KeyValuePair<String, String>> body = new List<KeyValuePair<String, String>>();
-            body.Add(new KeyValuePair<String, String>(ParamLatitude, Latitude));
-            body.Add(new KeyValuePair<String, String>(ParamLongitude, Longitude));
-            body.Add(new KeyValuePair<String, String>(ParamRadius, Radius));
-            body.Add(new KeyValuePair<String, String>(ParamLimit, Limit));
-            return SendWebRequest(Get, Offers, body);
+        public void GetDealersList() {
+            GetList(Dealers);
         }
 
-        public String GetStoreList() {
-
-            List<KeyValuePair<String, String>> body = new List<KeyValuePair<String, String>>();
-            body.Add(new KeyValuePair<String, String>(ParamLatitude, Latitude));
-            body.Add(new KeyValuePair<String, String>(ParamLongitude, Longitude));
-            body.Add(new KeyValuePair<String, String>(ParamRadius, Radius));
-            body.Add(new KeyValuePair<String, String>(ParamLimit, Limit));
-            return SendWebRequest(Get, Stores, body);
+        public void GetStoresList() {
+            GetList(Stores);
         }
 
-        public String GetDealerList() {
+        public void GetOffersList() {
+            GetList(Offers);
+        }
 
-            List<KeyValuePair<String, String>> body = new List<KeyValuePair<String, String>>();
-            body.Add(new KeyValuePair<String, String>(ParamLatitude, Latitude));
-            body.Add(new KeyValuePair<String, String>(ParamLongitude, Longitude));
-            body.Add(new KeyValuePair<String, String>(ParamRadius, Radius));
-            body.Add(new KeyValuePair<String, String>(ParamLimit, Limit));
-            return SendWebRequest(Get, Dealers, body); ;
+        private void GetList(string target) {
+
+            List<String> resultSet = new List<String>();
+            String result;
+            int offset = 0;
+
+            //Add desired parameters as headers for the eTilbudsavisen API
+            List<KeyValuePair<String, String>> query = new List<KeyValuePair<String, String>>();
+            query.Add(new KeyValuePair<String, String>(ParamLatitude, Latitude));
+            query.Add(new KeyValuePair<String, String>(ParamLongitude, Longitude));
+            query.Add(new KeyValuePair<String, String>(ParamRadius, Radius));
+            query.Add(new KeyValuePair<String, String>(ParamLimit, Limit));
+            query.Add(new KeyValuePair<String, String>(ParamOffset, offset.ToString()));
+
+            //Retrieve a result through the request
+            result = SendWebRequest(Get, target, query);
+
+            /*
+             * If result is valid, add it to the set of valid results.
+             * Keep sending requests and increase the offset to avoid duplicated results
+             * Stop when returned results are no longer valid
+             */
+            while (!String.IsNullOrEmpty(result)) {
+                resultSet.Add(result);
+                offset += Int32.Parse(Limit);
+                query[query.Count-1] = new KeyValuePair<String, String>(ParamOffset, offset.ToString());
+                result = SendWebRequest(Get, target, query);
+            }
         }
 
         private String SendWebRequest(String method, String extension, List<KeyValuePair<String, String>> arguments) {
 
             try {
-                //Set properties of the request
-                var request = (HttpWebRequest)WebRequest.Create(HostAddress + extension);
+                String finalAddress = HostAddress + extension;
+
+                //Add query to Address (if applicable)
+                if (method.Equals(Get)) {
+                    finalAddress += '?';
+                    finalAddress += arguments[0].Key + '=' + arguments[0].Value;
+                    for (int i = 1; i < arguments.Count; i++) {
+                        finalAddress += '&' + arguments[i].Key + '=' + arguments[i].Value;
+                    }
+                }
+
+                //Create request and set mandatory header properties
+                var request = (HttpWebRequest)WebRequest.Create(finalAddress);
                 request.Method = method;
                 request.ContentType = ContentType;
                 request.Accept = ContentType;
 
+                //If a Session Token and Signature are available (= After session create), add as headers
                 if (!String.IsNullOrEmpty(XToken)) {
                     request.Headers.Add(HeaderXToken, XToken);
                     request.Headers.Add(HeaderXSignature, XSignature);
                 }
-                
-                //Create JSON string containing the desired body arguments
+
+                //Create JSON string containing the desired body arguments (if applicable)
                 if (method.Equals(Post)) {
 
                     //Write body to API
