b7fa297b1bbf131e381b0d56b6a0d2a7c1c33618
Morten <treeline@live.dk>
2015-04-23 10:42:31 +0200

    New GUI for frontend

---
+++ sw8program/app/src/main/java/program/sw8/sw8program/RecommendActivity.java
@@ -0,0 +1,14 @@
+package program.sw8.sw8program;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RecommendActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend);
+    }
+}
--- sw8program/app/src/main/java/program/sw8/sw8program/RecommendFragment.java
+++ sw8program/app/src/main/java/program/sw8/sw8program/RecommendFragment.java
@@ -5,17 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RecommendFragment extends Fragment {
-    private TextView LinkToPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_recommend, container, false);
 
-        LinkToPreferences = (TextView) rootView.findViewById(R.id.link_preferences);
-        LinkToPreferences.setOnClickListener(onLinkClickListener);
+        TextView linkToPreferences = (TextView) rootView.findViewById(R.id.link_preferences);
+        Button buttonRecommend = (Button) rootView.findViewById(R.id.button_recommend);
+        linkToPreferences.setOnClickListener(onLinkClickListener);
 
 
         return rootView;
@@ -26,6 +27,14 @@ public class RecommendFragment extends Fragment {
         public void onClick(View v) {
             //Change the active page to the profile page
             ((PagerActivity)getActivity()).requestPageChange(0);
+        }
+    };
+
+    Button.OnClickListener onRecommendClickListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: Get recommendation
+
         }
     };
 }
